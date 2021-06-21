c362a855f97d22c0ee1114d44df803abc1ef04d0
Albin Groen <albin.groen@gmail.com>
2021-06-21 11:29:02 +0200

    :sparkles: Setup code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,87 @@
 export default function Home() {
   return (
-    <p>Hello, World</p>
-  )
+    <div className="overflow-y-auto">
+      <div className="min-h-screen px-8 py-12 mx-auto bg-white dark:bg-[#22272E] prose dark:prose-light max-w-screen-md">
+        <h1>Typography.js</h1>
+        <blockquote>
+          A powerful toolkit for building websites with beautiful design.
+        </blockquote>
+        <h2>Install</h2>
+        <code>npm install typography</code>
+        <h2>Why</h2>
+        <p>
+          Typography is a complex system of interrelated styles. 100s of style
+          declarations on dozens of elements must be in harmonious order. Trying
+          one design change can mean making dozens of tedious recalculations and
+          CSS value changes. Creating new Typography themes with CSS feels hard.
+        </p>
+        <p>
+          Typography.js provides a vastly simpler way to define and explore
+          typography designs. You provide configuration to the Typography.js JS
+          api and it uses its Typography engine to generate CSS for block and
+          inline elements.
+        </p>
+        <p>
+          Typography.js makes it easy to create designs that are unique,
+          personal, and custom to your project.
+        </p>
+
+        <h3>Testing</h3>
+        <p>Hello world</p>
+
+        <h2>Themes & Plugins</h2>
+        <ul>
+          <li>
+            <strong>themes</strong>: Typography.js themes are simple Javascript
+            objects. As such they’re easy to share across projects or even open
+            source and share via NPM.
+          </li>
+
+          <li>
+            <strong>plugins</strong>: Plugins are functions that extend or
+            modify the core Typography engine. They can change how headers are
+            styled or add specialized styles e.g. for code or tables.
+          </li>
+        </ul>
+
+        <h2>Sites that use Typography.js</h2>
+        <ul>
+          <li>
+            <a href="">bricolage.io (source)</a>
+          </li>
+          <li>
+            <a href="">React Headroom (source)</a>
+          </li>
+          <li>
+            <a href="">Gatsby Blog Starter (source)</a>
+          </li>
+          <li>
+            <a href="">ollieglass.com</a>
+          </li>
+          <li>
+            <a href="">Edit this file to add yours!</a>
+          </li>
+        </ul>
+
+        <h2>Javascript usage</h2>
+
+        <pre>{`import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.666,
+  headerFontFamily: ['Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Georgia', 'serif'],
+  // See below for the full list of options.
+})
+
+// Output CSS as string.
+typography.toString()
+
+// Or insert styles directly into the <head> (works well for client-only
+// JS web apps.
+typography.injectStyles()`}</pre>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,128 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        light: {
+          css: {
+            color: "#adbac7",
+
+            h1: {
+              color: theme("colors.gray.300"),
+              bordercolor: "#373e47",
+            },
+
+            h2: {
+              color: theme("colors.gray.300"),
+              bordercolor: "#373e47",
+            },
+
+            h3: {
+              color: theme("colors.gray.300"),
+            },
+
+            h4: {
+              color: theme("colors.gray.300"),
+            },
+
+            h5: {
+              color: theme("colors.gray.300"),
+            },
+
+            a: {
+              color: "#539bf5",
+            },
+
+            code: {
+              color: theme("colors.gray.300"),
+              backgroundcolor: "#3c4149",
+            },
+
+            pre: {
+              backgroundcolor: theme("colors.gray.700"),
+              backgroundcolor: "#2d333b",
+            },
+
+            strong: {
+              color: theme("colors.gray.300"),
+            },
+
+            blockquote: {
+              borderleftcolor: "#4f5864",
+              collor: "#768390",
+            },
+          },
+        },
+
+        default: {
+          css: {
+            collor: "#41464a",
+
+            h1: {
+              fontweight: theme("fontweight.semibold"),
+              borderbottom: `1px solid #ebedef`,
+              paddingbottom: theme("spacing.3"),
+              marginbottom: theme("spacing.0"),
+            },
+
+            h2: {
+              fontweight: theme("fontweight.semibold"),
+              borderbottom: `1px solid #ebedef`,
+              paddingbottom: theme("spacing.2"),
+              marginbottom: theme("spacing.3"),
+              margintop: theme("spacing.6"),
+            },
+
+            p: {
+              marginbottom: theme("spacing.3"),
+              margintop: theme("spacing.3"),
+            },
+
+            strong: {
+              fontweight: theme("fontweight.semibold"),
+            },
+
+            code: {
+              borderradius: theme("borderradius.default"),
+              fontweight: theme("fontweight.normal"),
+              background: theme("colors.gray.100"),
+              padding: theme("spacing.1"),
+              "&::before": {
+                display: "none",
+              },
+              "&::after": {
+                display: "none",
+              },
+            },
+
+            a: {
+              color: "#0066d6",
+              textdecoration: "none",
+              "&:hover": {
+                textdecoration: "underline",
+              },
+            },
+
+            li: {
+              marginbottom: theme("spacing.1"),
+              margintop: theme("spacing.1"),
+            },
+
+            blockquote: {
+              fontweight: theme("fontweight.normal"),
+              borderleftcolor: "#e0e3e6",
+              fontstyle: "none",
+              color: "#6a737d",
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
