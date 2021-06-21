e04d6fd6e2f4ebaa842d05d75b87134775e81349
Albin Groen <albin.groen@gmail.com>
2021-06-21 11:56:40 +0200

    :bug: Fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,12 @@ module.exports = {
 
             h1: {
               color: theme("colors.gray.300"),
-              bordercolor: "#373e47",
+              borderColor: "#373e47",
             },
 
             h2: {
               color: theme("colors.gray.300"),
-              bordercolor: "#373e47",
+              borderColor: "#373e47",
             },
 
             h3: {
@@ -37,12 +37,11 @@ module.exports = {
 
             code: {
               color: theme("colors.gray.300"),
-              backgroundcolor: "#3c4149",
+              backgroundColor: "#3c4149",
             },
 
             pre: {
-              backgroundcolor: theme("colors.gray.700"),
-              backgroundcolor: "#2d333b",
+              backgroundColor: "#2d333b",
             },
 
             strong: {
@@ -50,43 +49,43 @@ module.exports = {
             },
 
             blockquote: {
-              borderleftcolor: "#4f5864",
-              collor: "#768390",
+              borderLeftColor: "#4f5864",
+              color: "#768390",
             },
           },
         },
 
-        default: {
+        DEFAULT: {
           css: {
-            collor: "#41464a",
+            color: "#41464a",
 
             h1: {
-              fontweight: theme("fontweight.semibold"),
-              borderbottom: `1px solid #ebedef`,
-              paddingbottom: theme("spacing.3"),
-              marginbottom: theme("spacing.0"),
+              fontWeight: theme("fontWeight.semibold"),
+              borderBottom: `1px solid #ebedef`,
+              paddingBottom: theme("spacing.3"),
+              marginBottom: theme("spacing.0"),
             },
 
             h2: {
-              fontweight: theme("fontweight.semibold"),
-              borderbottom: `1px solid #ebedef`,
-              paddingbottom: theme("spacing.2"),
-              marginbottom: theme("spacing.3"),
-              margintop: theme("spacing.6"),
+              fontWeight: theme("fontWeight.semibold"),
+              borderBottom: `1px solid #ebedef`,
+              paddingBottom: theme("spacing.2"),
+              marginBottom: theme("spacing.3"),
+              marginTop: theme("spacing.6"),
             },
 
             p: {
-              marginbottom: theme("spacing.3"),
-              margintop: theme("spacing.3"),
+              marginBottom: theme("spacing.3"),
+              marginTop: theme("spacing.3"),
             },
 
             strong: {
-              fontweight: theme("fontweight.semibold"),
+              fontWeight: theme("fontWeight.semibold"),
             },
 
             code: {
-              borderradius: theme("borderradius.default"),
-              fontweight: theme("fontweight.normal"),
+              borderRadius: theme("borderRadius.default"),
+              fontWeight: theme("fontWeight.normal"),
               background: theme("colors.gray.100"),
               padding: theme("spacing.1"),
               "&::before": {
@@ -99,21 +98,21 @@ module.exports = {
 
             a: {
               color: "#0066d6",
-              textdecoration: "none",
+              textDecoration: "none",
               "&:hover": {
-                textdecoration: "underline",
+                textDecoration: "underline",
               },
             },
 
             li: {
-              marginbottom: theme("spacing.1"),
-              margintop: theme("spacing.1"),
+              marginBottom: theme("spacing.1"),
+              marginTop: theme("spacing.1"),
             },
 
             blockquote: {
-              fontweight: theme("fontweight.normal"),
-              borderleftcolor: "#e0e3e6",
-              fontstyle: "none",
+              fontWeight: theme("fontWeight.normal"),
+              borderLeftColor: "#e0e3e6",
+              fontStyle: "none",
               color: "#6a737d",
             },
           },
